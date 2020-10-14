a3b52be4ada7f22c297028e5e57c2366f59b9451
Alex Recker <alex@reckerfamily.com>
2020-10-14 00:35:38 -0500

    More platforms

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -3,7 +3,7 @@
 set -e
 
 # PLATFORMS="linux/386,linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64"
-PLATFORMS="linux/arm/v7"
+PLATFORMS="linux/386,linux/amd64,linux/arm/v7"
 
 docker_build() {
     docker buildx build \
