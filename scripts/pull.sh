0d39c2f0fb19f38af7a8b7d324b658016a3d9376
Alex Recker <alex@reckerfamily.com>
2021-10-25 21:29:51 -0500

    Fix python stuff

--- scripts/pull.sh
+++ scripts/pull.sh
@@ -5,9 +5,6 @@ log() {
     echo "pull.sh: $1" 1>&2
 }
 
-log 'installing latest gphotos-sync'
-~/.pyenv/shims/pip install --upgrade gphotos-sync
-
 log 'validating that $WALLPAPER_SECRETS is set'
 if [[ -z "${WALLPAPER_SECRETS}" ]]; then
     log 'error!  $WALLPAPER_SECRETS is not set!'
@@ -21,7 +18,7 @@ if ! test -f "$WALLPAPER_SECRETS"; then
 fi
 
 log 'pulling photos from album'
-~/.local/bin/gphotos-sync \
+gphotos-sync \
     --album "Wallpaper" \
     --use-hardlinks \
     --omit-album-date \
